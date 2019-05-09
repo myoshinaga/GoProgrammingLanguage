0bc1a10afb2aca6ce5be86c064e1d809c86d7e5a
myoshinaga <mana.yoshinaga1113@gmail.com>
2019-05-10 00:57:48 +0900

    add file 3

--- Chapter6/2/main.go
+++ Chapter6/2/main.go
@@ -9,6 +9,37 @@ type IntSet struct {
 	words []uint64
 }
 
+// http://cuto.unirita.co.jp/gostudy/post/go-operator/
+func (s *IntSet) IntersectWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) DifferenceWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] ^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) SymmetricDifference(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] &^= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
 func (s *IntSet) AddAll(xs ...int) {
 	for _, x := range xs {
 		s.Add(x)
